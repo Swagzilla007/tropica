d1f59cc66d38fafe4d6d872f9fc234ba4053e9ca
Swagzilla007 <158685149+Swagzilla007@users.noreply.github.com>
2025-03-24 20:37:43 +0530

    updated the sql file

--- hotel.sql
+++ hotel.sql
@@ -1,24 +1,16 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: May 18, 2017 at 05:44 PM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 7.0.13
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Database: `id1709610_hotel`
+-- Database: `hotel`
 --
+CREATE DATABASE IF NOT EXISTS `hotel` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `hotel`;
 
 -- --------------------------------------------------------
 
@@ -27,58 +19,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `manager` (
-  `uid` int(20) NOT NULL,
-  `uname` varchar(30) NOT NULL,
-  `upass` varchar(30) NOT NULL,
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `uname` varchar(100) NOT NULL,
+  `upass` varchar(100) NOT NULL,
   `fullname` varchar(100) NOT NULL,
-  `uemail` varchar(30) NOT NULL
+  `uemail` varchar(100) NOT NULL,
+  PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `manager`
+-- Default admin credentials
 --
 
 INSERT INTO `manager` (`uid`, `uname`, `upass`, `fullname`, `uemail`) VALUES
-(1, 'mamun', '1234', 'Abdullah Al Mamun', 'mamunwitchbug@gmail.com'),
-(3, 'jinat', 'jinat', 'Jinat Afroj', 'afrojjinat@gmail.com'),
-(6, 'admin', '1234', 'admin', 'admin@admin.com');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `rooms`
---
-
-CREATE TABLE `rooms` (
-  `room_id` int(200) NOT NULL,
-  `room_cat` text NOT NULL,
-  `checkin` date NOT NULL,
-  `checkout` date NOT NULL,
-  `name` text NOT NULL,
-  `phone` int(100) NOT NULL,
-  `book` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `rooms`
---
-
-INSERT INTO `rooms` (`room_id`, `room_cat`, `checkin`, `checkout`, `name`, `phone`, `book`) VALUES
-(23, 'Family', '0000-00-00', '0000-00-00', '', 0, 'false'),
-(24, 'Family', '0000-00-00', '0000-00-00', '', 0, 'false'),
-(25, 'Family', '0000-00-00', '0000-00-00', '', 0, 'false'),
-(26, 'Family', '0000-00-00', '0000-00-00', '', 0, 'false'),
-(27, 'Family', '0000-00-00', '0000-00-00', '', 0, 'false'),
-(28, 'Super Comfort', '2017-05-19', '2017-05-22', 'Jinat Afroj', 1524587558, 'true'),
-(29, 'Super Comfort', '0000-00-00', '0000-00-00', '', 0, 'false'),
-(30, 'Super Comfort', '0000-00-00', '0000-00-00', '', 0, 'false'),
-(31, 'Super Comfort', '0000-00-00', '0000-00-00', '', 0, 'false'),
-(32, 'Super Comfort', '0000-00-00', '0000-00-00', '', 0, 'false'),
-(33, 'Duplex', '0000-00-00', '0000-00-00', '', 0, 'false'),
-(34, 'Duplex', '0000-00-00', '0000-00-00', '', 0, 'false'),
-(35, 'Duplex', '0000-00-00', '0000-00-00', '', 0, 'false'),
-(36, 'Duplex', '0000-00-00', '0000-00-00', '', 0, 'false'),
-(37, 'Duplex', '0000-00-00', '0000-00-00', '', 0, 'false');
+(1, 'admin', 'admin', 'Administrator', 'admin@hotel.com');
 
 -- --------------------------------------------------------
 
@@ -87,61 +41,52 @@ INSERT INTO `rooms` (`room_id`, `room_cat`, `checkin`, `checkout`, `name`, `phon
 --
 
 CREATE TABLE `room_category` (
-  `roomname` text NOT NULL,
+  `roomname` varchar(100) NOT NULL,
   `room_qnty` int(11) NOT NULL,
   `available` int(11) NOT NULL,
   `booked` int(11) NOT NULL,
   `no_bed` int(11) NOT NULL,
-  `bedtype` text NOT NULL,
-  `facility` text NOT NULL,
-  `price` float NOT NULL
+  `bedtype` varchar(100) NOT NULL,
+  `facility` varchar(500) NOT NULL,
+  `price` float NOT NULL,
+  PRIMARY KEY (`roomname`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `room_category`
+-- Sample room categories
 --
 
 INSERT INTO `room_category` (`roomname`, `room_qnty`, `available`, `booked`, `no_bed`, `bedtype`, `facility`, `price`) VALUES
-('Duplex', 5, 5, 0, 2, 'single', 'AC, TV, Wifi', 1500),
-('Family', 5, 5, 0, 2, 'double', 'Sofa, TV, WIFI, Balcony, AC.', 3500),
-('Super Comfort', 5, 5, 0, 1, 'double', 'AC, TV, WIFI', 2200);
+('Luxury Suite', 5, 5, 0, 2, 'double', 'AC, WiFi, Mini Bar, Ocean View, Room Service', 15000),
+('Deluxe Room', 8, 8, 0, 1, 'double', 'AC, WiFi, City View', 8000),
+('Standard Room', 10, 10, 0, 1, 'single', 'AC, WiFi', 5000);
+
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- Table structure for table `rooms`
 --
 
---
--- Indexes for table `manager`
---
-ALTER TABLE `manager`
-  ADD PRIMARY KEY (`uid`);
+CREATE TABLE `rooms` (
+  `room_id` int(11) NOT NULL AUTO_INCREMENT,
+  `room_cat` varchar(100) NOT NULL,
+  `checkin` date DEFAULT NULL,
+  `checkout` date DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `phone` varchar(100) DEFAULT NULL,
+  `book` varchar(100) NOT NULL,
+  PRIMARY KEY (`room_id`),
+  FOREIGN KEY (`room_cat`) REFERENCES `room_category`(`roomname`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Indexes for table `rooms`
---
-ALTER TABLE `rooms`
-  ADD PRIMARY KEY (`room_id`);
-
---
--- Indexes for table `room_category`
---
-ALTER TABLE `room_category`
-  ADD PRIMARY KEY (`roomname`(100));
-
---
--- AUTO_INCREMENT for dumped tables
+-- Generate initial rooms based on room categories
 --
 
---
--- AUTO_INCREMENT for table `manager`
---
-ALTER TABLE `manager`
-  MODIFY `uid` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
---
--- AUTO_INCREMENT for table `rooms`
---
-ALTER TABLE `rooms`
-  MODIFY `room_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `rooms` (`room_cat`, `book`) 
+SELECT rc.roomname, 'false' 
+FROM room_category rc 
+CROSS JOIN (SELECT 1 AS n UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9 UNION SELECT 10) numbers 
+WHERE numbers.n <= rc.room_qnty;
+
+COMMIT;
